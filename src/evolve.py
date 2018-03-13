8aa6c52aabc7717b42b2b8e7d59c2aa6ad8d0f2c
Katherine Door <alevnyaa@gmail.com>
2018-03-13 12:29:03 +0100

    Skeleton evolver

---
+++ src/evolve.py
@@ -0,0 +1,24 @@
+from deep_nn import DeepNN
+from demo_deep_nn import demo
+
+class Evolver:
+    def __init__(self):
+        _models = []
+        _scores = []
+        _nn = DeepNN
+
+    def first_generation(self):
+        pass
+
+    def next_generation(self):
+        pass
+
+    def selection(self):
+        sort
+
+
+def main():
+    Evolver()
+
+if __name__ == '__main__':
+    main()
